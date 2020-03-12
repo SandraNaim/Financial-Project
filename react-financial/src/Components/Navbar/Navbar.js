9461e80e71bdc71a51afb912fc9febd53ad3e1aa
SandraNaim <naimsandra9@gmail.com>
2020-03-12 20:02:17 +0200

    change the color

--- react-financial/src/Components/Navbar/Navbar.js
+++ react-financial/src/Components/Navbar/Navbar.js
@@ -5,18 +5,18 @@ function Navbar() {
     return (
        
  
-    <header className="header_Navbar">
+    <header className="header_Navbar" style={{color:"white"}}>
         
         <ul className="container_Navbar">
             
-            <i className="fa fa-money"  aria-hidden="true"></i>
+            <i className="fa fa-money" aria-hidden="true"></i>
 
             <div className="name_Navbar"><p>H E R O</p></div>
       
            <nav  className="nav_Navbar">
                <li></li>
-            <li><a className="link_Navbar" href="#">Log-In </a></li>
-            <li><a className="link_Navbar" href="#">Register</a></li>
+            <li><a className="link_Navbar" style={{color:"white"}} href="#">Log-In </a></li>
+            <li><a className="link_Navbar" style={{color:"white"}}  href="#">Register</a></li>
             </nav> 
         </ul>
        
