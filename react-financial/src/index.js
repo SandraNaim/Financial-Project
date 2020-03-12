c550a4c0ae5ad4ccfba6d7be16a8c98569eeea05
SandraNaim <naimsandra9@gmail.com>
2020-03-12 15:41:39 +0200

    Login page Component

--- react-financial/src/index.js
+++ react-financial/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ToolNavbar from './Components/ToolNavbar/ToolNavbar';
+import LoginPage from './Components/LoginPage/LoginPage';
 
-ReactDOM.render(<ToolNavbar />, document.getElementById('root'));
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
