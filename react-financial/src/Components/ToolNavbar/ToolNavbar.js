88c64d900d5d0b772eccc16db563c8d0b2703e03
SandraNaim <naimsandra9@gmail.com>
2020-03-12 13:13:54 +0200

    tool navbar components

---
+++ react-financial/src/Components/ToolNavbar/ToolNavbar.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+
+function ToolNavbar() {
+    return (
+
+<SideNav
+    onSelect={(selected) => {
+        // Add your code here
+    }}
+>
+    <SideNav.Toggle /> 
+    <SideNav.Nav defaultSelected="dashboard">
+        <NavItem eventKey="dashboard">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Dashboard
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="Transaction">
+            <NavIcon>
+                <i className="fa fa-money" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Transaction
+            </NavText>
+            <NavItem eventKey="transaction/incomes">
+                <NavText>
+                    Incomes
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="transaction/expenses">
+                <NavText>
+                    Expenses
+                </NavText>
+            </NavItem>
+        </NavItem>
+        <NavItem eventKey="reports">
+            <NavIcon>
+                <i className="fa fa-bar-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Reports
+            </NavText>
+        </NavItem>
+        
+        <NavItem eventKey="setting" style={{marginTop:"200px"}}>
+            <NavIcon>
+                <i className="fa fa-cog fa-fw" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Setting
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="help">
+            <NavIcon>
+                <i className="fa fa-book fa-fw" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Help
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="about">
+            <NavIcon>
+                <i className="fa fa-pencil fa-fw" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                About
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
+
+    )
+}
+
+
+export default ToolNavbar;
+
--- react-financial/src/index.js
+++ react-financial/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ToolNavbar from './Components/ToolNavbar/ToolNavbar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToolNavbar />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
