2ca018fbca9a4997daf4a8194ec3eea82119ef4f
KarimFarra <Elfarrakarim@gmail.com>
2020-03-14 14:55:01 +0200

    fixing

---
+++ react-financial/src/Components/Income_Card/Income_Card.js
@@ -0,0 +1,235 @@
+import React from "react";
+import "./Income_Card.css";
+
+function Income_Card() {
+  return (
+    <div className="container-fluid" >
+      <div className="row">
+        <div className="col-1"></div>
+        <div className="col-sm-10 ">
+          <div className="card mt-20">
+            <h5 className="card-header text-center " style={{ backgroundColor: 'rgb(54, 54, 112)', color: 'white' }}>Income Card</h5>
+            <div className="card-body">
+              <table className="table">
+                <thead className="" style={{ backgroundColor: '', border: '2px solid #dee2e6' }}>
+                  <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Categary</th>
+                    <th scope="col">Currency</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">
+                      <input type="title" className="form-control" aria-describedby="emailHelp" />
+                    </th>
+                   
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>Finance</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><input type="title" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td><input type="date" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td>
+                      <div className="form-group">
+
+                        <textarea className="form-control" ></textarea>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input type="title" className="form-control" aria-describedby="emailHelp" />
+                    </th>
+                   
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>Finance</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><input type="title" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td><input type="date" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td>
+                      <div className="form-group">
+
+                        <textarea className="form-control" ></textarea>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input type="title" className="form-control" aria-describedby="emailHelp" />
+                    </th>
+                   
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>Finance</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><input type="title" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td><input type="date" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td>
+                      <div className="form-group">
+
+                        <textarea className="form-control" ></textarea>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input type="title" className="form-control" aria-describedby="emailHelp" />
+                    </th>
+                   
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>Finance</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><input type="title" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td><input type="date" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td>
+                      <div className="form-group">
+
+                        <textarea className="form-control" ></textarea>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input type="title" className="form-control" aria-describedby="emailHelp" />
+                    </th>
+                   
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>Finance</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="form-group">
+
+                        <select className="form-control" id="exampleFormControlSelect1">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td><input type="title" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td><input type="date" className="form-control" aria-describedby="emailHelp" /></td>
+                    <td>
+                      <div className="form-group">
+
+                        <textarea className="form-control" ></textarea>
+                      </div>
+                    </td>
+                  </tr>
+                    </tbody>
+              </table>
+
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary" style={{ backgroundColor: 'rgb(54, 54, 112)', }}>+ ADD INCOME</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+
+  )
+
+}
+
+export default Income_Card;
--- react-financial/src/index.js
+++ react-financial/src/index.js
@@ -5,8 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ToolNavbar from './Components/ToolNavbar/ToolNavbar';
 import Navbar from './Components/Navbar/Navbar';
+import Income_Card from './Components/Income_Card/Income_Card';
 
-ReactDOM.render(<Navbar />, document.getElementById('root'));   
+
+ReactDOM.render(<Income_Card/>, document.getElementById('root'));   
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
