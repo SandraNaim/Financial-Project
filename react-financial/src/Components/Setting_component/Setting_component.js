40f75fea801e8f24300b957cf74f3db63c37023c
SandraNaim <naimsandra9@gmail.com>
2020-03-24 10:54:55 +0200

    adding on setting page

--- react-financial/src/Components/Setting_component/Setting_component.js
+++ react-financial/src/Components/Setting_component/Setting_component.js
@@ -12,17 +12,45 @@ class Setting_component extends React.Component {
     render(){
     return (
 
-    <div className="container">
+    <div className="container"style={{ marginBottom:"3%"}}>
         <h3>General Setting</h3>
+        <br/> <br/>
+        <div className="row" style={{ marginLeft:"3%"}}>
+            <div className="col-md-10 setting_one">
+                <h6>Change your Name</h6>
+                <div className="form-group" style={{ marginLeft:"15%",marginRight:"15%"}}>
+
+                    <input disabled="disabled" style={{width:"300px"}} type="text" class="form-control" id="username" placeholder="Enter your name"/>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-primary" style={{ backgroundColor: 'rgb(54, 54, 112)',height:"40px" }}>Update</button>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <br/>
+        <div className="row" style={{ marginLeft:"3%"}}>
+            <div className="col-md-10 setting_one">
+                <h6>Change your Password</h6>
+                <div className="form-group " style={{ marginLeft:"12%",marginRight:"15%"}}>
+
+                    <input disabled="disabled" style={{width:"300px"}} type="text" class="form-control" id="userpassword" placeholder="Enter your password"/>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-primary" style={{ backgroundColor: 'rgb(54, 54, 112)',height:"40px" }}>Update</button>
+                </div>
+            </div>
+        </div>
+        <hr/>
         <br/>
         <div className="row " style={{marginTop:"3%", marginLeft:"3%"}}>
-            <div className="col-md-6 setting_one">
+            <div className="col-md-10 setting_one">
                 <h6>Base Currency</h6>
-                <div className="form-group " >
+                <div className="form-group " style={{ marginLeft:"20%"}}>
                    
-                    <div className="input-group"style={{width:"280px"}} >
+                    <div className="input-group"style={{width:"300px"}} >
                                            
-                        <select class="form-control" id="currency">
+                        <select class="form-control" id="currency" >
                             <option>$</option>
                             <option>L.L</option>
                             <option>AED</option>
@@ -38,7 +66,7 @@ class Setting_component extends React.Component {
         <div className="row" style={{ marginLeft:"3%"}}>
             <div className="col-md-10 setting_one">
                 <h6>Income Category</h6>
-                <div className="form-group ">
+                <div className="form-group " style={{ marginLeft:"17%"}}>
 
                     <input disabled="disabled" style={{width:"300px"}} type="text" class="form-control" id="incomecat" placeholder="Enter your income category"/>
                 
@@ -55,8 +83,8 @@ class Setting_component extends React.Component {
         <br/>
         <div className="row" style={{ marginLeft:"3%"}}>
             <div className="col-md-10 setting_one">
-                <h6>Expenses Category</h6>
-                <div className="form-group ">
+                <h6>Expense Category</h6>
+                <div className="form-group " style={{ marginLeft:"15%"}}>
 
                     <input disabled="disabled" style={{width:"300px"}} type="text" class="form-control" id="expensecat" placeholder="Enter your expense category"/>
                 
